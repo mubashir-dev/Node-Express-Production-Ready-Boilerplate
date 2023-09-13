63be3225ac4ccdbaffa4fb7620c383166ba60a68
Mubashir Ali <mubashirmobi633@gmail.com>
2023-09-14 01:46:18 +0500

    feat(setup):tweaked global error middleware

--- src/config/response.config.js
+++ src/config/response.config.js
@@ -13,28 +13,8 @@ const responseHandler = (data) => {
     });
 };
 
-const globalErrorHandler = (err, request, response, next) => {
-    const originalError = err;
-    if (!(err instanceof customError)) {
-        if (err instanceof Error) {
-            err = new customError({
-                message: err.message,
-            });
-        }
-    }
-    console.error('error', `URL: ${request.url}`, { meta: { error: originalError.stack, body: request } });
 
-    return responseHandler({
-        response,
-        message: err.message,
-        result: null,
-        code: err.code || 500,
-        errors: err.errors ?? err,
-        isSuccess: false,
-    });
-};
 
 module.exports = {
-    globalErrorHandler,
     responseHandler
 }
