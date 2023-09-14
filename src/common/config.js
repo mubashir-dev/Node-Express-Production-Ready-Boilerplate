b79bc19669653a5eb2bfceb0ce04387056a79b8e
AwaisR <awaisrazzaa@gmail.com>
2023-09-14 12:05:38 -0400

    chore(lint):fixed code linting

--- src/common/config.js
+++ src/common/config.js
@@ -1,27 +1,28 @@
-require("dotenv").config();
+require('dotenv').config();
 
-
-module.exports = config = {
-    appMode: process.env.NODE_ENV || 'development',
-    apiVersion: process.env.API_VERSION || "v1",
-    nodeServicePort: process.env.NODE_SERVICE_PORT || 4000,
-    database: {
-        host: process.env.MYSQL_DB_HOST,
-        user: process.env.MYSQL_DB_USER,
-        password: process.env.MYSQL_DB_PASSWORD,
-        name: process.env.MYSQL_DB_NAME,
-        logging: process.env.MYSQL_DB_LOGGING,
-        max: process.env.MYSQL_DB_POOL_MAX,
-        min: process.env.MYSQL_DB_POOL_MIN,
-        acquire: process.env.MYSQL_DB_POOL_MYSQL_DB_POOL_ACQUIRE,
-        idle: process.env.MYSQL_DB_POOL_MYSQL_DB_POOL_IDLE,
-    },
-    redis: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-    },
-    JWT: {
-        auth_token: process.env.JWT_AUTH,
-        timeout: process.env.JWT_TIMEOUT,
-    }
+const config = {
+  appMode: process.env.NODE_ENV || 'development',
+  apiVersion: process.env.API_VERSION || 'v1',
+  nodeServicePort: process.env.NODE_SERVICE_PORT || 4000,
+  database: {
+    host: process.env.MYSQL_DB_HOST,
+    user: process.env.MYSQL_DB_USER,
+    password: process.env.MYSQL_DB_PASSWORD,
+    name: process.env.MYSQL_DB_NAME,
+    logging: process.env.MYSQL_DB_LOGGING,
+    max: process.env.MYSQL_DB_POOL_MAX,
+    min: process.env.MYSQL_DB_POOL_MIN,
+    acquire: process.env.MYSQL_DB_POOL_MYSQL_DB_POOL_ACQUIRE,
+    idle: process.env.MYSQL_DB_POOL_MYSQL_DB_POOL_IDLE,
+  },
+  redis: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  JWT: {
+    authToken: process.env.JWT_AUTH,
+    timeout: process.env.JWT_TIMEOUT,
+  },
 };
+
+module.exports = config;
--- src/middlewares/request.middleware.js
+++ src/middlewares/request.middleware.js
@@ -1,18 +1,18 @@
-const { logger } = require("../utils/logger.util");
+const { logger } = require('../utils/logger.util');
 
 const RequestLogger = (request, response, next) => {
-    if (!response.finished) {
-        const { ip, method, url } = request;
-        const { statusCode } = response;
-        const hostname = require('os').hostname();
-        const userAgent = request.get('user-agent') || '';
-        logger.info(
-            `[hostname: ${hostname}] [method : ${method}] [url: ${url}] [status : ${statusCode}] [userAgent:[${userAgent}] Ip:[${ip}]]`
-        );
-    }
-    next();
+  if (!response.finished) {
+    const { ip, method, url } = request;
+    const { statusCode } = response;
+    const hostname = require('os').hostname();
+    const userAgent = request.get('user-agent') || '';
+    logger.info(
+      `[hostname: ${hostname}] [method : ${method}] [url: ${url}] [status : ${statusCode}] [userAgent:[${userAgent}] Ip:[${ip}]]`,
+    );
+  }
+  next();
 };
 
 module.exports = {
-    RequestLogger
-}
+  RequestLogger,
+};
